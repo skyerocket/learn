9fd18ea5279022061f3d1225b3d728d269e84e1f
Yang Fei <yang.fei@educationhorizons.com>
2021-07-04 21:23:55 +1000

    typo

--- javascript/week1-client.js
+++ javascript/week1-client.js
@@ -34,8 +34,7 @@ class Request {
 				})
 			}
 			connection.on('data', data=>{
-				console.log(data.toString())
-				parser.recieve(data.toString())
+				parser.receive(data.toString())
 				if(parser.isFinished) {
 					resolve(parser.response)
 					connection.end()
@@ -49,7 +48,9 @@ class Request {
 	}
 
 	toString() {
-		return `${this.method} ${this.path} HTTP/1.1\r ${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`.join('\r\n'))}\r\r ${this.bodyText}`
+		return `${this.method} ${this.path} HTTP/1.1\r
+		${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r \r 
+		${this.bodyText}`
 	}
 }
 
@@ -71,6 +72,21 @@ class ResponseParser {
 		this.headerValue = ''
 		this.bodyParser = null
 	}
+
+	get isFinished() {
+		return this.bodyParser && this.bodyParser.isFinished
+	}
+
+	get response() {
+		this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
+		return {
+			statusCode: RegExp.$1,
+			statusText: RegExp.$2,
+			headers: this.headers,
+			body: this.bodyParser.content.join('')
+		}
+	}
+
 	receive(string) {
 		for (let i = 0; i < string.length; i++) {
 			this.receiveChar(string.charAt(i))
@@ -92,6 +108,8 @@ class ResponseParser {
 				this.current = this.WAITING_HEADER_SPACE
 			} else if (char === '\r') {
 				this.current = this.WAITING_HEADER_BLOCK_END
+				if(this.headers['Transfer-Encoding'] === 'chunked') 
+					this.bodyParser = new TrunkedBodyParser()
 			} else {
 				this.headerName += char
 			}
@@ -117,7 +135,53 @@ class ResponseParser {
 				this.current = this.WAITING_BODY
 			}
 		} else if (this.current === this.WAITING_BODY) {
-			console.log(char)
+			this.bodyParser.receiveChar(char)
+		}
+	}
+}
+
+class TrunkedBodyParser {
+	constructor() {
+		this.WAITING_LENGTH = 0
+		this.WAITING_LENGTH_LINE_END = 1
+		this.READING_TRUNK = 2
+		this.WAITING_NEW_LINE = 3
+		this.WAITING_NEW_LINE_END = 4
+		this.length = 0
+		this.content = []
+		this.isFinished = false
+		this.current = this.WAITING_LENGTH
+	}
+
+	receiveChar(char) {
+		if(this.current === this.WAITING_LENGTH) {
+			if (char === '\r') {
+				if(this.length ===0) {
+					this.isFinished = true
+				}
+				this.current = this.WAITING_LENGTH_LINE_END
+			} else {
+				this.length *= 16
+				this.length += parseInt(char, 16)
+			}
+		} else if (this.current === this.WAITING_LENGTH_LINE_END) {
+			if (char === '\n') {
+				this.current = this.READING_TRUNK
+			}
+		} else if (this.current === this.READING_TRUNK) {
+			this.content.push(char)
+			this.length --
+			if (this.length === 0) {
+				this.current = this.WAITING_NEW_LINE
+			}
+		} else if (this.current === this.WAITING_NEW_LINE) {
+			if (char === '\r') {
+				this.current = this.WAITING_NEW_LINE_END
+			}
+		} else if (this.current === this.WAITING_NEW_LINE_END) {
+			if (char === '\n') {
+				this.current = this.WAITING_LENGTH
+			}
 		}
 	}
 }
